d79fa06e8fdd51b3d67324bfd356403b255ba0ff
Aryan Srivastava <Aryansrivas123@gmail.com>
2023-08-17 12:12:26 +0530

    Dice Functionalities

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 //Selecting elements
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
 const dice = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,4 +15,21 @@ score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add('hidden');
 
+let currentScore = 0;
+
 //Rolling Dice Functionalities
+btnRoll.addEventListener('click', function () {
+  //Generating a random dice roll
+  const score = Math.trunc(Math.random() * 6) + 1;
+
+  //Display Dice
+  dice.classList.remove('hidden');
+  dice.src = `dice-${score}.png`;
+
+  //When the dice roll is 1
+  if (score !== 1) {
+    currentScore += score;
+    current0.textContent = currentScore;
+  } else {
+  }
+});
