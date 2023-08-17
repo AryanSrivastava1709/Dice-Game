11acf3d02513128573b929a2e41f2fdf39d6520b
Aryan Srivastava <Aryansrivas123@gmail.com>
2023-08-18 02:19:21 +0530

    switching players

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
 const current0 = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling Dice Functionalities
 btnRoll.addEventListener('click', function () {
@@ -29,7 +33,13 @@ btnRoll.addEventListener('click', function () {
   //When the dice roll is 1
   if (score !== 1) {
     currentScore += score;
-    current0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
 });
